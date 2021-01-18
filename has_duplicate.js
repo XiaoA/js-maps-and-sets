b1f2ae5f4d8ebff39ff706019104a79330cd6f75
Andrew Buckingham <abuckingham99@gmail.com>
2021-01-17 16:47:11 -0800

    Implement hasDuplicate() function

---
+++ has_duplicate.js
@@ -0,0 +1,11 @@
+function hasDuplicate(array) {
+  const duplicateSet = new Set(array);
+  if (array.length !== duplicateSet.size) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+hasDuplicate([1, 3, 2, 1]) // true
+hasDuplicate([1, 5, -1, 4]) // false
